c5e1a02a74e5e59732c9d272a4c7336c792803a6
SANTHIYA K <krishnansanthiya07@gmail.com>
2026-08-04 21:29:37 +0530

    projectdescription.py

---
+++ projectdescription.py
@@ -0,0 +1,23 @@
+Project Description
+The Travel Management System is a Python-based console application designed to simplify travel planning. It allows users to register, log in, explore destinations, book hotels and transport, calculate trip expenses, and manage bookings through an interactive and user-friendly interface.
+
+Features
+- User Registration & Login
+- Destination Search
+- Hotel Booking
+- Transport Booking
+- Budget Calculator
+- Booking Management
+
+Technologies Used
+- Python
+- Functions
+- Loops
+- Conditional Statements
+- File Handling (if used)
+
+Future Scope
+- Online Payment Integration
+- Real-Time Booking Updates
+- Mobile Application
+- AI-Based Travel Recommendations
